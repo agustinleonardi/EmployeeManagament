c6521623394bc3f1ea5e2057feecb56fb0651d0b
agustinleonardi <agustinleonardi5@gmail.com>
2025-09-10 18:07:53 -0300

    add: SonarQube

--- EmployeeManagement.Application/obj/Debug/net8.0/EmployeeManagement.Application.AssemblyInfo.cs
+++ EmployeeManagement.Application/obj/Debug/net8.0/EmployeeManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39fd181cdf50ef8194cb9111564bf4e2588d0c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
